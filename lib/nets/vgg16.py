55af978199101922fbe14163d6e805a9bc287c02
yangfan <1150941991@qq.com>
2018-01-08 10:33:08 +0800

    load conv parameters

--- lib/nets/vgg16.py
+++ lib/nets/vgg16.py
@@ -32,44 +32,59 @@ class vgg16(Network):
         initializer_bbox = tf.random_normal_initializer(mean=0.0, stddev=0.001)
 
       #thermal sub_net
+      #with tf.device('/gpu:1'):
       net1 = slim.repeat(self._imageT, 2, slim.conv2d, 64, [3, 3],
-                        trainable=False, scope='conv1T')
+                         trainable=False, scope='conv1T')
+      #net1 = slim.conv2d(net1, 64, [1, 1],scope='NIN_T1')
       net1 = slim.max_pool2d(net1, [2, 2], padding='SAME', scope='pool1T')
       net1 = slim.repeat(net1, 2, slim.conv2d, 128, [3, 3],
-                        trainable=False, scope='conv2T')
+                         trainable=False, scope='conv2T')
+      #net1 = slim.conv2d(net1, 128, [1, 1],scope='NIN_T2')
       net1 = slim.max_pool2d(net1, [2, 2], padding='SAME', scope='pool2T')
       net1 = slim.repeat(net1, 3, slim.conv2d, 256, [3, 3],
-                        trainable=is_training, scope='conv3T')
+                         trainable=is_training, scope='conv3T')
+      #net1 = slim.conv2d(net1, 256, [1, 1],scope='NIN_T3')
       net1 = slim.max_pool2d(net1, [2, 2], padding='SAME', scope='pool3T')
       net1 = slim.repeat(net1, 3, slim.conv2d, 512, [3, 3],
-                        trainable=is_training, scope='conv4T')
-      net1 = slim.max_pool2d(net1, [2, 2], padding='SAME', scope='pool4T')
-      net1 = slim.repeat(net1, 3, slim.conv2d, 512, [3, 3],
-                        trainable=is_training, scope='conv5T')
+                         trainable=is_training, scope='conv4T')
+      #net1 = slim.max_pool2d(net1, [2, 2], padding='SAME', scope='pool4T')
+      #net1 = slim.repeat(net1, 3, slim.conv2d, 512, [3, 3],
+      #                   trainable=is_training, scope='conv5T')
+
       #RGB sub_net
       net2 = slim.repeat(self._imageRGB, 2, slim.conv2d, 64, [3, 3],
                          trainable=False, scope='conv1RGB')
+      #net2 = slim.conv2d(net2, 64, [1, 1],scope='NIN_RGB1')
       net2 = slim.max_pool2d(net2, [2, 2], padding='SAME', scope='pool1RGB')
       net2 = slim.repeat(net2, 2, slim.conv2d, 128, [3, 3],
                          trainable=False, scope='conv2RGB')
+      #net2 = slim.conv2d(net2, 128, [1, 1],scope='NIN_RGB2')
       net2 = slim.max_pool2d(net2, [2, 2], padding='SAME', scope='pool2RGB')
       net2 = slim.repeat(net2, 3, slim.conv2d, 256, [3, 3],
                          trainable=is_training, scope='conv3RGB')
+      #net2 = slim.conv2d(net2, 256, [1, 1],scope='NIN_RGB3')
       net2 = slim.max_pool2d(net2, [2, 2], padding='SAME', scope='pool3RGB')
       net2 = slim.repeat(net2, 3, slim.conv2d, 512, [3, 3],
                          trainable=is_training, scope='conv4RGB')
-      net2 = slim.max_pool2d(net2, [2, 2], padding='SAME', scope='pool4RGB')
-      net2 = slim.repeat(net2, 3, slim.conv2d, 512, [3, 3],
-                         trainable=is_training, scope='conv5RGB')
+      #net2 = slim.max_pool2d(net2, [2, 2], padding='SAME', scope='pool4RGB')
+      #net2 = slim.repeat(net2, 3, slim.conv2d, 512, [3, 3],
+      #                   trainable=is_training, scope='conv5RGB')
 
-      net = tf.add(0.5*net1, 0.5*net2)
+      pdb.set_trace()
+      net = tf.concat([net1,net2],3,name='concat')
+      net = slim.conv2d(net, 512, [1, 1],scope='NIN1')
+      net = slim.conv2d(net, 512, [1, 1],scope='NIN2')
+      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3],
+                        trainable=is_training, scope='conv5')
 
-      #pdb.set_trace()
+      #net = tf.add(0.5*net1, 0.5*net2)
+
       self._act_summaries.append(net)
       self._layers['head'] = net
       # build the anchors for the image
       self._anchor_component()
       # region proposal network
+      #pdb.set_trace()
       rois = self._region_proposal(net, is_training, initializer)
       # region of interest pooling
       if cfg.POOLING_MODE == 'crop':
@@ -77,6 +92,7 @@ class vgg16(Network):
       else:
         raise NotImplementedError
 
+      #fully connected layer
       pool5_flat = slim.flatten(pool5, scope='flatten')
       fc6 = slim.fully_connected(pool5_flat, 4096, scope='fc6')
       if is_training:
@@ -84,7 +100,12 @@ class vgg16(Network):
       fc7 = slim.fully_connected(fc6, 4096, scope='fc7')
       if is_training:
         fc7 = slim.dropout(fc7, keep_prob=0.5, is_training=True, scope='dropout7')
+
+      #global average pooling
+      #fc7 = slim.avg_pool2d(pool5, [7, 7],padding='VALID',scope='avg_pool')
+      
       # region classification
+      #pdb.set_trace()
       cls_prob, bbox_pred = self._region_classification(fc7,
                                                         is_training,
                                                         initializer,
@@ -94,41 +115,52 @@ class vgg16(Network):
 
       return rois, cls_prob, bbox_pred
 
-  def get_variables_to_restore(self, variables, var_keep_dic):
+  def get_variables_to_restore(self, variables, var_keep_dic_T, var_keep_dic_RGB):
     #variables_to_restore = []
-    variables_to_restore = {}
+    T_variables_to_restore = {}
+    RGB_variables_to_restore = {}
+
+    #for v in variables:
+    #  # exclude the conv weights that are fc weights in vgg16
+    #  if v.name == 'vgg_16/fc6/weights:0' or v.name == 'vgg_16/fc7/weights:0':
+    #    self._variables_to_fix[v.name] = v
+    #    continue
+    #  # exclude the first conv layer to swap RGB to BGR
+    #  if v.name == 'vgg_16/conv1RGB/conv1RGB_1/weights:0':
+    #    self._variables_to_fix[v.name.replace('RGB','')] = v
+    #    continue
+    #  v1 = v.name.split(':')[0]
+    #  if v1.replace('T','') in var_keep_dic:
+    #    print('Variables restored: %s' % v.name)
+    #    T_variables_to_restore[v1.replace('T','')]=v
+    #    continue
+    #  if v1.replace('RGB','') in var_keep_dic:
+    #    print('Variables restored: %s' % v.name)
+    #    RGB_variables_to_restore[v1.replace('RGB','')]=v
+    #    continue
 
     for v in variables:
-      # exclude the conv weights that are fc weights in vgg16
-      if v.name == 'vgg_16/fc6/weights:0' or v.name == 'vgg_16/fc7/weights:0':
-        self._variables_to_fix[v.name] = v
-        continue
-      # exclude the first conv layer to swap RGB to BGR
-      if v.name == 'vgg_16/conv1T/conv1T_1/weights:0':
-        self._variables_to_fix[v.name.replace('T','')] = v
-        continue
-      #if v.name == 'vgg_16/conv1RGB/conv1RGB_1/weights:0':
-      #  self._variables_to_fix[v.name.replace('RGB','')] = v
-      #  continue
+        # exclude the conv weights that are fc weights in vgg16
+        if v.name == 'vgg_16/fc6/weights:0' or v.name == 'vgg_16/fc7/weights:0':
+            self._variables_to_fix[v.name] = v
+            continue
+        # exclude the first conv layer to swap RGB to BGR
+        if not(v.name[0:11]==u'vgg_16/conv'):
+            continue
+        if v.name == 'vgg_16/conv1RGB/conv1RGB_1/weights:0':
+            self._variables_to_fix[v.name.replace('RGB','')] = v
+            continue
+        v1 = v.name.split(':')[0]
+        if v1.replace('T','') in var_keep_dic_T:
+            print('Variables restored: %s' % v.name)
+            T_variables_to_restore[v1.replace('T','')]=v
+            continue
+        if v1.replace('RGB','') in var_keep_dic_RGB:
+            print('Variables restored: %s' % v.name)
+            RGB_variables_to_restore[v1.replace('RGB','')]=v
+            continue
 
-      #if v.name.split(':')[0] in var_keep_dic:
-      #  print('Variables restored: %s' % v.name)
-      #  variables_to_restore.append(v)
-      #if v.name == 'vgg_16/fc6/biases:0' or v.name == 'vgg_16/fc7/biases:0':
-      #  v1 = v.name.split(':')[0]
-      #  print('Variables restored: %s' % v.name)
-      #  variables_to_restore[v1]=v
-      #  continue
-      v1 = v.name.split(':')[0]
-      if v1.replace('T','') in var_keep_dic:
-        print('Variables restored: %s' % v.name)
-        variables_to_restore[v1.replace('T','')]=v
-      #if v1.replace('RGB','') in var_keep_dic:
-      #  print('Variables restored: %s' % v.name)
-      #  variables_to_restore[v1.replace('RGB','')]=v
-
-
-    return variables_to_restore
+    return T_variables_to_restore,RGB_variables_to_restore
 
   def fix_variables(self, sess, pretrained_model):
     print('Fix VGG16 layers..')
